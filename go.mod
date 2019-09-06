50c502c842e1f0f56ca84f3c1d402c314f5da2a2
tuweiguang <tuweiguang@foxmail.com>
2019-09-06 16:07:49 +0800

    go mod test

---
+++ go.mod
@@ -0,0 +1,8 @@
+module GoTestApi
+
+go 1.13
+
+require (
+	github.com/tuweiguang/GoTestLib v0.0.5
+	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297
+)
--- test.go
+++ test.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+        "fmt"
+        api "github.com/tuweiguang/GoTestLib/api"
+        context "golang.org/x/net/context"
+)
 
 func main() {
-    fmt.Println("hello GoTestApi")
+    api.MyApi()
+
+    c := context.Background()
+    if c == nil {
+	fmt.Println("Background returned nil")
+    }
+    select {
+        case x := <-c.Done():
+	    fmt.Printf("<-c.Done() == %v want nothing (it should block)", x)
+        default:
+    }
+	
+    if got, want := fmt.Sprint(c), "context.Background"; got != want {
+	fmt.Printf("Background().String() = %q want %q", got, want)
+    }else{
+        fmt.Println(got,want)
+    }
+
+    fmt.Println("end")
 }
