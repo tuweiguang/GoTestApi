d6cf82f3eacf8cc9ea28c04cb2fb7738bd986eae
tuweiguang <tuweiguang@bianfeng.com>
2020-06-22 15:17:48 +0800

    test jenkins

--- test.go
+++ test.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-        "fmt"
-        api "github.com/tuweiguang/GoTestLib/api"
-        context "golang.org/x/net/context"
+	"fmt"
+	api "github.com/tuweiguang/GoTestLib/api"
+	context "golang.org/x/net/context"
 )
 
 func main() {
-    api.MyApi()
+	api.MyApi()
 
-    c := context.Background()
-    if c == nil {
-	fmt.Println("Background returned nil")
-    }
-    select {
-        case x := <-c.Done():
-	    fmt.Printf("<-c.Done() == %v want nothing (it should block)", x)
-        default:
-    }
-	
-    if got, want := fmt.Sprint(c), "context.Background"; got != want {
-	fmt.Printf("Background().String() = %q want %q", got, want)
-    }else{
-        fmt.Println(got,want)
-    }
+	c := context.Background()
+	if c == nil {
+		fmt.Println("Background returned nil")
+	}
+	select {
+	case x := <-c.Done():
+		fmt.Printf("<-c.Done() == %v want nothing (it should block)", x)
+	default:
+	}
 
-    fmt.Println("end")
+	if got, want := fmt.Sprint(c), "context.Background"; got != want {
+		fmt.Printf("Background().String() = %q want %q", got, want)
+	} else {
+		fmt.Println(got, want)
+	}
+
+	fmt.Println("end")
+
+	fmt.Println("hello jenkins")
 }
